dc04c8cadcb2593bd5a0791676061e2c7e70f698
Isaiah Binford <Binfordi14@students.ecu.edu>
2018-02-14 18:31:19 -0500

    Create stringConcatenation.js

---
+++ JavaScript_Code/stringConcatenation.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: Yosa Buson (2-6)
+ * Directions:
+ * Build a string using concatenation by combining the lines from this famous haiku poem by Yosa Buson.
+
+ * Blowing from the west
+ * Fallen leaves gather
+ * In the east.
+ *
+ * Each string should be printed on its own line.
+ */
+
+var lineOne = "Blowing from the west\n";
+var lineTwo = "Fallen leaves gather\n";
+var lineThree = "In the east.";
+
+var haiku = lineOne + lineTwo + lineThree;
+console.log(haiku);
