f85a9fa1120b4418601f7a84e98a2b0f628d05a9
Isaiah Binford <Binfordi14@students.ecu.edu>
2018-02-14 18:14:27 -0500

    Create stringQuiz.js

---
+++ JavaScript_Code/stringQuiz.js
@@ -0,0 +1,8 @@
+/*
+ * Directions:
+ * Create a string with the name of your favorite food. The first letter of the string should be capitalized.
+ * Programming Quiz: Favorite Food (2-3)
+ */
+
+var favoriteFood = "Pizza";
+console.log(favoriteFood);
