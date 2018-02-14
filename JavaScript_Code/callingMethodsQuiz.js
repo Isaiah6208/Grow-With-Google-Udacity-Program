e21e4ebee3a57dd11c41c221160594254a3c21af
Isaiah Binford <Binfordi14@students.ecu.edu>
2018-02-14 18:59:56 -0500

    Create callingMethodsQuiz.js

---
+++ JavaScript_Code/callingMethodsQuiz.js
@@ -0,0 +1,18 @@
+/*
+* Directions:
+* Create a variable called bill and assign it the result of 10.25 + 3.99 + 7.15
+* (don't perform the calculation yourself, let JavaScript do it!).
+* Next, create a variable called tip and assign it the result of multiplying
+* bill by a 15% tip rate. Finally, add the bill and tip together and store it
+* into a variable called total.
+
+* Print the total to the JavaScript console.
+
+ * Programming Quiz: Out to Dinner (2-10)
+ */
+
+var bill = 10.25 + 3.99 + 7.15;
+var tip = bill * .15;
+var total = tip + bill;
+
+console.log("$" + total.toFixed(2));
