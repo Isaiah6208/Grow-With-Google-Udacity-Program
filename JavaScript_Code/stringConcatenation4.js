9cbf46b3da9954aefdfd337d5b964e55110da83b
Isaiah Binford <Binfordi14@students.ecu.edu>
2018-02-14 19:16:34 -0500

    Create stringConcatenation4.js

---
+++ JavaScript_Code/stringConcatenation4.js
@@ -0,0 +1,20 @@
+/*
+ * Programming Quiz: One Awesome Message (2-12)
+ *
+ * 1. Create the variables:
+ *     - firstName
+ *     - interest
+ *     - hobby
+ * 2. Create a variable named awesomeMessage and, using string concatenation 
+      and the variables above, create an awesome message.
+ * 3. Print the awesomeMessage variable to the console.
+ */
+ 
+var firstName = "Isaiah";
+var interest = "food";
+var hobby = "complain";
+
+var awesomeMessage = "Hi, my name is " + firstName + ". I love " + interest +
+ ". In my spare time, I like to " + hobby + ".";
+ 
+console.log(awesomeMessage);
