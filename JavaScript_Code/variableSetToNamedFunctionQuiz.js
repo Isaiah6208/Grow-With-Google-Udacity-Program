5401b27e039be6049d8b4eec0910a4b278ddf4a1
Isaiah Binford <Binfordi14@students.ecu.edu>
2018-02-16 17:08:40 -0500

    Create variableSetToNamedFunctionQuiz.js

---
+++ JavaScript_Code/variableSetToNamedFunctionQuiz.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Cry (5-5)
+ * Directions:
+ * Write a named function expression that stores the function
+ * in a variable called cry and returns "boohoo!".
+ */
+
+var cry = function cryEveryDay(){
+    var boohoo = "boohoo!";
+    return boohoo;
+}
+
+console.log(cry);
