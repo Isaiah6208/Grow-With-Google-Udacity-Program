3d5fd4a20afb1faf4b1bee32579f35220e9f3567
Isaiah Binford <Binfordi14@students.ecu.edu>
2018-02-17 00:07:19 -0500

    Update createAnArrayQuiz2.js

--- JavaScript_Code/createAnArrayQuiz2.js
+++ JavaScript_Code/createAnArrayQuiz2.js
@@ -1,5 +1,8 @@
 /*
  * Programming Quiz: Building the Crew (6-2)
+ * Directions:
+ * Create an array called crew to organize the Serenity’s crew and set it equal to the variables below.
+ * You don't need to type out the actual strings, just use the provided variables.
  */
 
 var captain = "Mal";
